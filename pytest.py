6c7cd3ddcc053bd83f61f2768fc00dedf3b1236f
otrewyi191 <piofcircle@gmail.com>
2018-05-11 15:10:48 +0000

    Init

---
+++ pytest.py
@@ -0,0 +1,11 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+
+# Create .env file path.
+dotenv_path = join(dirname(__file__), '.env')
+
+# Load file from the path.
+load_dotenv(dotenv_path)
+
+print(os.environ.get('name'))
